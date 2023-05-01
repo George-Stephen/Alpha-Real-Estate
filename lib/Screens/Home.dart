4ad67fd927148df62f3ed345281621e4b642a85c
George Stephen <georgesteve561@gmail.com>
2023-05-01 16:57:08 +0300

    start development

---
+++ lib/Screens/Home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
